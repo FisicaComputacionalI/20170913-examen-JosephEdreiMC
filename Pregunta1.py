02497b7505fd5abde87a5bca8f1885551c07caa3
JosephEdreiMC <joseph.edrei.mc@gmail.com>
2017-09-13 13:05:24 -0500

    Create Pregunta1.py
    Primer ejercicio del primer examen parcial, 13 de septiembre 2017.

---
+++ Pregunta1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+t = np.arange (0.0, 20.0, 1.0)
+plt.plot (t, 1997+t, 'g--')
+plt.ylabel('anio')
+plt.xlabel('edad')
+plt.title('Joseph_Edrei_Moreno_Cruz')
+plt.savefig('Pregunta1')
+plt.show()
